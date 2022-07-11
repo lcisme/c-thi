1935562fe19f518314dfbd0fe20c49c66eefc09a
lcisme <92872523+lcisme@users.noreply.github.com>
2022-07-11 10:30:50 +0700

    thi

---
+++ Pratice_Exam/Pratice_Exam/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Pratice_Exam
+{
+    public class Mainn
+    {
+        public static void Main(string[] args)
+        {
+            Product pd = new Product();
+            while (true)
+            {
+            Console.WriteLine("\nChoose the function you want");
+            Console.WriteLine("1. Add product records");
+            Console.WriteLine("2. Display product records");
+            Console.WriteLine("3. Delete product by Id");
+            Console.WriteLine("4. Exit");
+            int x = Convert.ToInt32(Console.ReadLine());    
+                switch (x)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Add product records");
+                            pd.addpd();
+                            Console.WriteLine("Add successfully");
+                            break;
+                        }
+                    case 2:
+                        {
+
+                            Console.WriteLine("Display product records");
+                            pd.displaypd(pd.getProduct());
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Delete");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Exit");
+                            return;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Pratice_Exam/Pratice_Exam/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Pratice_Exam
+{
+    class Product
+    {
+        private List<Product> list = null;
+        
+        public Product()
+        {
+            list = new List<Product>();
+        }
+        public string id { get; set; }
+        public string productname { get; set; }
+        public string price { get; set; }
+
+        public void addpd()
+        {
+            Product add = new Product();
+            Console.WriteLine("Id");
+            add.id = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Product Name");
+            add.productname = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Price");
+            add.price = Convert.ToString(Console.ReadLine());
+
+            list.Add(add);
+        }
+        public void displaypd(List<Product> listpd)
+        {
+            Console.WriteLine("{0, -30} {1, -30} {2, -30}",
+                            "Product ID", "Product Name", "Price");
+            if (listpd != null && listpd.Count > 0)
+            {
+                foreach (Product pd in listpd)
+                {
+                    Console.WriteLine("{0, -30} {1, -30} {2, -30}",
+                                      pd.id, pd.productname, pd.price);
+                }
+            }
+            Console.WriteLine();
+        }
+        public List<Product> getProduct()
+        {
+            return list;
+        }
+
+        public bool deletepd()
+        {
+            Console.WriteLine("Id");
+        }
+    }
+}
